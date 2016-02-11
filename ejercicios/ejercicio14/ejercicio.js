f6c6242310470e425b93240ca6d7ca1b792ff0f9
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2016-02-11 21:46:09 +0100

    Limpiado ejercicio 15

--- ejercicios/ejercicio14/ejercicio.js
+++ ejercicios/ejercicio14/ejercicio.js
@@ -3,19 +3,6 @@
     .module('directivas')
     .directive('ejercicioHeroes4', function (){
       const ddo = {
-        scope:{
-          equipo: '='
-        },
-        controller: () => {},
-        controllerAs: 'vm',
-        bindToController: true,
-        template: `
-          <ul>
-            <li ng-repeat="heroe in vm.equipo">
-              {{heroe.nombre}}
-            </li>
-          </ul>
-        `
       };
       return ddo;
     });
